c1179d0bc6fa67bcbb4cfa585fa307a96ca74a4d
Heisenberg <75937169+heisenberg-88@users.noreply.github.com>
2021-06-10 15:56:59 +0530

    fetching repo names in BLUE log window with multi threading

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from os import name
 from tkinter.constants import DISABLED, END, RIGHT, Y
+from tkinter.font import names
 from github import Github
 import tkinter as gui
+import threading
 
 
 root=gui.Tk()
@@ -9,6 +12,8 @@ root.title("Github-TraningData-Collector-for-pythonTransformers")
 root.geometry('700x450')
 
 access_token_string="invalid"
+
+
 
 ##Below code is for fetching the github access token from gui
 token_var=gui.StringVar()
@@ -20,6 +25,7 @@ token_entry = gui.Entry(root,textvariable = token_var, font=('calibre',10,'norma
 token_entry.config(show="*")
 token_entry.pack(ipadx=200)
 
+output = gui.Text(root, height = 20, width = 85, background = "blue")
 
 
 def getaccess():
@@ -28,25 +34,27 @@ def getaccess():
 
     ghtoken=Github(str(access_token_string))
 
-    print(ghtoken.get_user())
 
+    # output.insert(END,f"{ghtoken.get_user()}\n")
     query="language:python"
     result=ghtoken.search_repositories(query)
-    print(result.totalCount)
 
+    # output.insert(END,f"{result.totalCount}")
+    
     for file in result:
-        print(file.name)
-
-
-
-btnRead=gui.Button(root, height=1, width=20, text="Read Access Token", command=getaccess)
+        output.insert('1.0',f"{file.name}\n")
+        output.config(state=DISABLED)
+        output.pack()
+        
+## making thread for smooth UI interface while background code running
+def threadcreation():
+    t=threading.Thread(target=getaccess)
+    t.start()
+        
+btnRead=gui.Button(root, height=1, width=20, text="Read Access Token", command=threadcreation )
 btnRead.configure(activebackground="yellow")
 btnRead.pack(pady=5)
 
-output = gui.Text(root, height = 20, width = 85, background = "blue")
-output.insert(END,"Hello World","bold")
-output.config(state=DISABLED)
-output.pack()
 
 root.mainloop()
 
